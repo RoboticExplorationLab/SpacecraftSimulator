bf00c689b6e640b6b40f47892d3aa3120aec3572
Kevin Tracy <ktracy@stanford.edu>
2020-11-12 13:05:53 -0800

    some linting

--- tle_parser/ecef_eci_Stuff.jl
+++ tle_parser/ecef_eci_Stuff.jl
@@ -49,7 +49,7 @@ end
 
 
 
-function myECItoECEF(seconds_from_9_1_2020_to_epoch,seconds_from_epoch)
+function myECItoECEF(seconds_from_9_1_2020_to_epoch, seconds_from_epoch)
     """ECI to ECEF rotation matrix.
 
     Args:
@@ -60,7 +60,7 @@ function myECItoECEF(seconds_from_9_1_2020_to_epoch,seconds_from_epoch)
         ECEF_Q_ECI: DCM
     """
     t = seconds_from_9_1_2020_to_epoch + seconds_from_epoch
-    return rotz(5.940294244015959 + t*7.292115146706979e-5)
+    return rotz(5.940294244015959 + t * 7.292115146706979e-5)
 
     # t = seconds_from_9_1_2020_to_epoch + seconds_from_epoch
     # ang_64 = 5.940294244015959 + t*7.292115146706979e-5
@@ -73,25 +73,25 @@ end
 
 epoch_0 = Epoch("2020-09-01")
 
-epoch_anchor = Epoch("2020-09-14") -12323.3423
+epoch_anchor = Epoch("2020-09-14") - 12323.3423
 # test a few of these
-one_month = 24*3600*30.32434 # seconds
+one_month = 24 * 3600 * 30.32434 # seconds
 
 
 # epc2 = epoch_0 +dt
 seconds_from_9_1_2020_to_epoch = epoch_anchor - epoch_0
 
-seconds_from_epoch = rand(-100:.00001:50)*one_month
+seconds_from_epoch = rand(-100:.00001:50) * one_month
 
 Q = rECItoECEF(epoch_anchor + seconds_from_epoch)
-Qest = myECItoECEF(seconds_from_9_1_2020_to_epoch,seconds_from_epoch )
+Qest = myECItoECEF(seconds_from_9_1_2020_to_epoch, seconds_from_epoch)
 
-error_phi_deg = rad2deg(norm(phi_from_dcm(Q'*Qest)))
+error_phi_deg = rad2deg(norm(phi_from_dcm(Q' * Qest)))
 
 @show error_phi_deg
-@show max_error_distance_km = 1.08*R_EARTH*2*pi*(error_phi_deg/360)/1000
+@show max_error_distance_km = 1.08 * R_EARTH * 2 * pi * (error_phi_deg / 360) / 1000
 
-@show Q'*Qest
+@show Q' * Qest
 # theta_0 = 5.940294244015959
 #
 # omega = 7.292115146706979e-5
@@ -112,17 +112,17 @@ error_phi_deg = rad2deg(norm(phi_from_dcm(Q'*Qest)))
 
 # 7.292115146706979e-5
 
-t = 5.234*one_month
-w_earth_pt1 = convert(Float32,7.29211e-5)
-w_earth_pt2 = convert(Float32,.000005146706e-5)
+t = 5.234 * one_month
+w_earth_pt1 = convert(Float32, 7.29211e-5)
+w_earth_pt2 = convert(Float32, .000005146706e-5)
 
-t_32 = 5.234 * convert(Float32,one_month)
+t_32 = 5.234 * convert(Float32, one_month)
 
-ang_64 = t*OMEGA_EARTH % 2*pi
+ang_64 = t * OMEGA_EARTH % 2 * pi
 
-ang_32 = t_32*w_earth_pt1 % (2*convert(Float32,pi))
-ang_32 = t_32*w_earth_pt1 % (2*convert(Float32,pi)) + t_32*w_earth_pt2 % (2*convert(Float32,pi))
-ang_32 = t_32*w_earth_pt1 % 2*pi
+ang_32 = t_32 * w_earth_pt1 % (2 * convert(Float32, pi))
+ang_32 = t_32 * w_earth_pt1 % (2 * convert(Float32, pi)) + t_32 * w_earth_pt2 % (2 * convert(Float32, pi))
+ang_32 = t_32 * w_earth_pt1 % 2 * pi
 
 
 # julia is awesome
@@ -135,4 +135,4 @@ R_n_c = R_n_a * R_a_b * R_b_c
 R_c_to_n = R_a_to_n * R_b_to_a * R_c_to_b
 
 
-end
+
